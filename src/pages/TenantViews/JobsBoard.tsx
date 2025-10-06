0d72a18ff6b1ad1d9867c6b5c8f8f20e3abceef2
Greg Fielding <greg@fieldingdesigngroup.com>
2025-10-06 17:11:45 -0500

    fix(job-postings): correct spacing alignment for Pay Rate row

--- src/pages/TenantViews/JobsBoard.tsx
+++ src/pages/TenantViews/JobsBoard.tsx
@@ -769,27 +769,29 @@ const JobsBoard: React.FC = () => {
               />
             </Stack>
 
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Pay Rate ($/hr)"
-                  type="number"
-                  value={newPost.payRate}
-                  onChange={(e) => setNewPost({ ...newPost, payRate: e.target.value })}
-                  fullWidth
-                  inputProps={{ min: 0, step: 0.01 }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
-                  <Typography variant="body1">Show Pay Rate</Typography>
-                  <Switch
-                    checked={newPost.showPayRate}
-                    onChange={(e) => setNewPost({ ...newPost, showPayRate: e.target.checked })}
+            <Box sx={{ mt: 2 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label="Pay Rate ($/hr)"
+                    type="number"
+                    value={newPost.payRate}
+                    onChange={(e) => setNewPost({ ...newPost, payRate: e.target.value })}
+                    fullWidth
+                    inputProps={{ min: 0, step: 0.01 }}
                   />
-                </Box>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+                    <Typography variant="body1">Show Pay Rate</Typography>
+                    <Switch
+                      checked={newPost.showPayRate}
+                      onChange={(e) => setNewPost({ ...newPost, showPayRate: e.target.checked })}
+                    />
+                  </Box>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
 
             <FormControl fullWidth>
               <InputLabel>Visibility</InputLabel>
