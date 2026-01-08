56a1d519222f8a7d2a3c349948be00c619192a46
Greg Fielding <greg@fieldingdesigngroup.com>
2026-01-07 17:25:46 -0800

    Fix /contacts standalone TenantCRM by adding CRMCacheProvider

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { useAuth } from '../contexts/AuthContext';
+import { CRMCacheProvider } from '../contexts/CRMCacheContext';
 import RecruiterContacts from './RecruiterContacts';
 import TenantCRM from './TenantViews/TenantCRM';
 
@@ -14,7 +15,13 @@ const ContactsPage: React.FC = () => {
   const { crmSalesEnabled, recruiterEnabled } = useAuth();
 
   // Standalone master table view
-  if (crmSalesEnabled) return <TenantCRM standaloneTab="contacts" />;
+  if (crmSalesEnabled) {
+    return (
+      <CRMCacheProvider>
+        <TenantCRM standaloneTab="contacts" />
+      </CRMCacheProvider>
+    );
+  }
   if (recruiterEnabled) return <RecruiterContacts />;
 
   return (
